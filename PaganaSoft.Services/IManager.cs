8c72e996f8842860df25f587e015508373e921d1
amarodev <amarotux@gmail.com>
2014-09-06 21:36:15 +0200

    initial commit

---
+++ PaganaSoft.Services/IManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace PaganaSoft.Services
+{
+    [ServiceContract]
+    public interface IManager
+    {
+
+        [OperationContract]
+        string Greetings(string value);
+
+        [OperationContract]
+        Person CreatePerson(int id, string fname, string lname);
+    }
+
+
+    [DataContract]
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ PaganaSoft.Services/Manager.svc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace PaganaSoft.Services
+{
+    public class Manager : IManager
+    {
+
+        public string Greetings(string value)
+        {
+            return String.Format("Hola¡¡ {0}", value);
+        }
+
+
+        public Person CreatePerson(int id, string fname, string lname)
+        {
+            return new Person()
+            {
+                Id = id,
+                FirstName = fname,
+                LastName = lname
+            };
+        }
+    }
+}
